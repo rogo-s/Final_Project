a1e2bcbe8bb18c885ee94a6221ee564311fda027
Rogo <rogosubandono513@gmail.com>
2023-06-07 18:27:46 +0700

    ui home, notif,history, akun - rogo

--- app/src/main/java/com/rogo/final_project/view/MainActivity.kt
+++ app/src/main/java/com/rogo/final_project/view/MainActivity.kt
@@ -2,11 +2,59 @@ package com.rogo.final_project.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.rogo.final_project.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragContainer) as NavHostFragment
+
+        navController = navHostFragment.navController
+        bottomNav = findViewById(R.id.bottomNav)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.homeFragment2,
+            R.id.historyNonLoginFragment2,
+            R.id.notifFragment2,
+            R.id.akunNonLoginFragment2
+        ).build()
+
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNav.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener{_,item,_ ->
+            when(item.id){
+                R.id.homeFragment2 -> hideBottom(false)
+                R.id.historyNonLoginFragment2 -> hideBottom(false)
+                R.id.notifFragment2 -> hideBottom(false)
+                R.id.akunNonLoginFragment2 -> hideBottom(false)
+                else -> hideBottom(true)
+            }
+        }
+    }
+
+    private fun hideBottom(hide: Boolean) {
+        if (hide){
+            bottomNav.visibility = View.GONE
+        }else{
+            bottomNav.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/rogo/final_project/view/fragment/SplashFragment.kt
+++ app/src/main/java/com/rogo/final_project/view/fragment/SplashFragment.kt
@@ -1,10 +1,12 @@
 package com.rogo.final_project.view.fragment
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.rogo.final_project.R
 import com.rogo.final_project.databinding.FragmentSplashBinding
 
@@ -17,5 +19,13 @@ class SplashFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment2)
+        },3000)
     }
 }
