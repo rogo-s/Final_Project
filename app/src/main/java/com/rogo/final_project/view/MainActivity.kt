5cb638cc8b5080a598340ae03e46ee10afe7b87e
anzila26 <sitianzilanur@gmail.com>
2023-06-07 09:28:55 +0700

    add layout login,regist,sendotp,resetpass

--- app/src/main/java/com/rogo/final_project/view/MainActivity.kt
+++ app/src/main/java/com/rogo/final_project/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rogo.final_project
+package com.rogo.final_project.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rogo.final_project.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/rogo/final_project/view/fragment/ResetPasswordFragment.kt
@@ -0,0 +1,22 @@
+package com.rogo.final_project.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rogo.final_project.R
+import com.rogo.final_project.databinding.FragmentResetPasswordBinding
+
+class ResetPasswordFragment : Fragment() {
+    lateinit var binding : FragmentResetPasswordBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentResetPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
