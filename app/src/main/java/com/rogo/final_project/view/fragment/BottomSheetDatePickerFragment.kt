f16ffd0d510125ccc01cc6100d9e242713a9b449
Rogo <rogosubandono513@gmail.com>
2023-06-13 22:02:14 +0700

    commit

--- app/src/main/java/com/rogo/final_project/view/fragment/BottomSheetDatePickerFragment.kt
+++ app/src/main/java/com/rogo/final_project/view/fragment/BottomSheetDatePickerFragment.kt
@@ -9,6 +9,7 @@ import androidx.core.util.Pair
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.rogo.final_project.R
+import com.archit.calendardaterangepicker.customviews.CalendarListener
 import com.rogo.final_project.databinding.FragmentBottomSheetDatePickerBinding
 import java.text.SimpleDateFormat
 import java.util.*
@@ -34,26 +35,42 @@ class BottomSheetDatePickerFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.setDatePicker.setOnClickListener {
-            val datePickerRange = MaterialDatePicker.Builder.dateRangePicker()
-                .setTitleText("Select Date")
-                .setSelection(
-                    Pair(
-                        MaterialDatePicker.thisMonthInUtcMilliseconds(),
-                        MaterialDatePicker.todayInUtcMilliseconds()
-                    )
-                )
-                .build()
-            datePickerRange.show(requireActivity().supportFragmentManager,"date_picker")
+//        binding.setDatePicker.setOnClickListener {
+//            val datePickerRange = MaterialDatePicker.Builder.dateRangePicker()
+//                .setTitleText("Select Date")
+//                .setSelection(
+//                    Pair(
+//                        MaterialDatePicker.thisMonthInUtcMilliseconds(),
+//                        MaterialDatePicker.todayInUtcMilliseconds()
+//                    )
+//                )
+//                .build()
+//            datePickerRange.show(requireActivity().supportFragmentManager,"date_picker")
+//
+//            datePickerRange.addOnPositiveButtonClickListener {
+//                val simpleDateFormat = SimpleDateFormat("EEE, dd MMMM yyyy", Locale.getDefault())
+//                binding.setDatePicker.text = "${simpleDateFormat.format(it.first)}"
+//                binding.setDatePicker2.text = "${simpleDateFormat.format(it.second)}"
+//            }
+//        }
 
-            datePickerRange.addOnPositiveButtonClickListener {
-                val simpleDateFormat = SimpleDateFormat("EEE, dd MMMM yyyy", Locale.getDefault())
-                binding.setDatePicker.text = "${simpleDateFormat.format(it.first)}"
-                binding.setDatePicker2.text = "${simpleDateFormat.format(it.second)}"
+        binding.calendar.setCalendarListener(object : CalendarListener{
+            override fun onDateRangeSelected(startDate: Calendar, endDate: Calendar) {
+                val dateFormat = SimpleDateFormat("EEEE, d MMMM yyyy",Locale("id","ID"))
+                val startDateText = dateFormat.format(startDate.time)
+                val endDateText = dateFormat.format(endDate.time)
+                binding.setDatePicker.text = startDateText.toString()
+                binding.setDatePicker2.text = endDateText.toString()
+
+
             }
-        }
+
+            override fun onFirstDateSelected(startDate: Calendar) {
+
+            }
 
 
+        })
     }
 
 
