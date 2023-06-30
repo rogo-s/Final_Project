3ab5a9362162a111d778b2a87f92500e696341e4
BAGAS <anndptra658@gmail.com>
2023-06-30 16:28:45 +0700

    Update Round Trip Pencarian by Query, Hasil pencarian round trip by Query

---
+++ app/src/androidTest/java/com/rogo/final_project/codegen/_com_rogo_final_project_di_App.java
@@ -0,0 +1,19 @@
+package com.rogo.final_project.codegen;
+
+import dagger.hilt.android.HiltAndroidApp;
+import dagger.hilt.internal.aggregatedroot.AggregatedRoot;
+
+/**
+ * This class should only be referenced by generated code! This class aggregates information across multiple compilations.
+ */
+@AggregatedRoot(
+    root = "com.rogo.final_project.di.App",
+    rootPackage = "com.rogo.final_project.di",
+    originatingRoot = "com.rogo.final_project.di.App",
+    originatingRootPackage = "com.rogo.final_project.di",
+    rootAnnotation = HiltAndroidApp.class,
+    rootSimpleNames = "App",
+    originatingRootSimpleNames = "App"
+)
+public class _com_rogo_final_project_di_App {
+}
